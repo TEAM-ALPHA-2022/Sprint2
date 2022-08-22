1007a42875c1e5b0e427f223677f949a0d20fd7a
Pbaquero30 <111189188+Pbaquero30@users.noreply.github.com>
2022-08-21 20:19:34 -0500

    Modificacion

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] arg){
-        Empresa empresa=new Empresa("Hospital Departamental de Villacicencio","Calle 37A#28-11","663347","89200501");
-        System.out.println(empresa.getNombre_empresa());
-        System.out.println(empresa.getDireccion());
-        System.out.println(empresa.getTelefono());
-        System.out.println(empresa.getNit());
-        empresa.setNombre_empresa("Paola");
-        empresa.setDireccion("Calle 7b # 43-77");
-        empresa.setTelefono("3223078762");
-        empresa.setNit("1121958797");
+        System.out.println("hola");
     }
 }
