a8598c7a39c270dbfc959add5c2b9cf9a4e4e256
Pbaquero30 <111189188+Pbaquero30@users.noreply.github.com>
2022-08-21 20:16:15 -0500

    Creacion empresa

--- Empresa.java
+++ Empresa.java
@@ -1,2 +1,46 @@
 public class Empresa {
+    private String nombre_empresa;
+    private String direccion;
+
+    private String telefono;
+    private String nit;
+
+    public Empresa(String nombre_empresa, String direccion, String telefono, String nit) {
+        this.nombre_empresa = nombre_empresa;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nit = nit;
+    }
+
+    public String getNombre_empresa() {
+        return nombre_empresa;
+    }
+
+    public void setNombre_empresa(String nombre_empresa) {
+        this.nombre_empresa = nombre_empresa;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
 }
