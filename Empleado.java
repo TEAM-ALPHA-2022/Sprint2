012a7210c941873ce731748d3e658e2af50c2445
ANDRESDIAZ1103 <cesarandresdiazsanchez2016@gmail.com>
2022-08-22 21:09:04 -0500

    Creacion Empleado

--- Empleado.java
+++ Empleado.java
@@ -1,2 +1,10 @@
 public class Empleado {
+
+    private String nombre_empleado;
+    private String apellido_empleado;
+    private int cedula;
+    private int edad;
+    private String cargo;
+
+
 }
