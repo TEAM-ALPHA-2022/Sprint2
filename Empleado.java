c70c8bdc9cd42d939bc67d33891ee8cf88ebe873
ANDRESDIAZ1103 <cesarandresdiazsanchez2016@gmail.com>
2022-08-22 22:50:43 -0500

    Creacion Empleado

--- Empleado.java
+++ Empleado.java
@@ -1,10 +1,46 @@
 public class Empleado {
 
     private String nombre_empleado;
-    private String apellido_empleado;
-    private int cedula;
-    private int edad;
-    private String cargo;
+    private String email_empleado;
+    private String empresa_empleado;
+    private String rol_empleado;
 
+    public Empleado(String nombre_empleado, String email_empleado, String empresa_empleado, String rol_empleado) {
+        this.nombre_empleado = nombre_empleado;
+        this.email_empleado = email_empleado;
+        this.empresa_empleado = empresa_empleado;
+        this.rol_empleado = rol_empleado;
+    }
 
+    public String getNombre_empleado() {
+        return nombre_empleado;
+    }
+
+    public void setNombre_empleado(String nombre_empleado) {
+        this.nombre_empleado = nombre_empleado;
+    }
+
+    public String getEmail_empleado() {
+        return email_empleado;
+    }
+
+    public void setEmail_empleado(String email_empleado) {
+        this.email_empleado = email_empleado;
+    }
+
+    public String getEmpresa_empleado() {
+        return empresa_empleado;
+    }
+
+    public void setEmpresa_empleado(String empresa_empleado) {
+        this.empresa_empleado = empresa_empleado;
+    }
+
+    public String getRol_empleado() {
+        return rol_empleado;
+    }
+
+    public void setRol_empleado(String rol_empleado) {
+        this.rol_empleado = rol_empleado;
+    }
 }
