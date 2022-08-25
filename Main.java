f8ebfb4a7b0e5e6c70622a297a83873d9ccbde45
AndresFagua <111401068+AndresFagua@users.noreply.github.com>
2022-08-25 12:35:37 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] arg){
         System.out.println("hola");
+        public static void main(String ... args){
+     System.out.println("ejecutando codigo"); }
     }
 }
